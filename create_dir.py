0130af3e14ec5e0dd89e455e765917467b734d3a
pranav karuvally <pranavsdreams@gmail.com>
2019-10-04 23:07:59 +0530

    Created the file to create directories

---
+++ create_dir.py
@@ -0,0 +1,55 @@
+import os
+
+'''
+The working directory is the user_data 
+which is inside the Trash directory
+'''
+
+class file_management:
+    def __init__(self):
+        #check_list consists of all the required directories
+        self.check_list = {'Photos', 'Videos', 'Audio', 'Documents', 'Others'}
+        self.pic_form = ['.jpg', '.jpeg', '.png']
+        self.vid_form = ['.mp4', '.mkv']
+        self.doc_form = ['.pdf', '.txt']
+        self.aud_form = ['.wav', '.mp3']
+        self.pics = []
+        self.vids = []
+        self.docs = []
+        self.auds = []
+    
+    def check_dir(self):
+        #This check whether all elements in check_list are in the previous folder
+        if not self.check_list.issubset(set(os.listdir('..'))):
+            [os.makedirs('../'+x) for x in self.check_list]
+
+    #This function will move the files to the corresponding position
+    #matching the corresponding data types
+    def folder_shift(self, extensions, folder):
+        self.pics=[y for x in extensions for y in self.all_files if x in y]
+        [os.system('mv ../{} ../{}/'.format(x, folder)) for x in self.pics]
+    
+    def categorise(self):
+        self.all_files = set(os.listdir('..')) - self.check_list
+        self.all_files -= {'user_data'}
+
+        #These lines shift the files into the directories
+        self.folder_shift(self.pic_form, 'Photos')
+        self.folder_shift(self.vid_form, 'Videos')
+        self.folder_shift(self.aud_form, 'Audio')
+        self.folder_shift(self.doc_form, 'Documents')
+
+        for x in os.listdir('..'):
+            #This line moves all other files to the other directory
+            if os.path.isfile(x):
+                os.system('mv ../' + x + ' Others/')
+            else:
+                #This line moves all directories other than our default ones to Others
+                if x not in list(self.check_list) + ['user_data']:
+                    os.system('mv ../' + x + ' Others/')
+        
+
+a = file_management()
+
+a.check_dir()
+a.categorise()

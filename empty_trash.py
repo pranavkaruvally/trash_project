ef3285797f58f2f7786cb5885ded5cba7af91ab8
pranav karuvally <pranavsdreams@gmail.com>
2019-10-05 15:09:57 +0530

    Added some commenting

--- empty_trash.py
+++ empty_trash.py
@@ -3,9 +3,14 @@ from os import system, path
 
 def empty_trash():
     create_dir_class = File_management()
+    #The set check_list in the class File_management has
+    #the names of all the folders that were created
+    #and hence the names of all the folders to be deleted
     check_list = list(create_dir_class.check_list)
 
     for directory in check_list:
+        #This line checks whether each of the directories are present
+        #The function is actually used to check whether the file is a directory
         if path.isdir('../' + directory):
             system('rm -r ../{}'.format(directory))
 

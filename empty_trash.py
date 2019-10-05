f0eec5ee36176a026ff23676c6a6072746390f47
pranav karuvally <pranavsdreams@gmail.com>
2019-10-05 15:04:13 +0530

    Added the code inside a function

--- empty_trash.py
+++ empty_trash.py
@@ -1,9 +1,12 @@
 from create_dir import File_management
 from os import system
 
-if __name__ == '__main__':
+def empty_trash():
     create_dir_class = File_management()
     check_list = list(create_dir_class.check_list)
 
     for directory in check_list:
         system('rm -r ../{}'.format(directory))
+
+if __name__ == '__main__':
+    empty_trash()

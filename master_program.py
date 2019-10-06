3675ef2b18c7e2b428b984d6e71b89dd848f8462
pranav karuvally <pranavsdreams@gmail.com>
2019-10-06 18:03:35 +0530

    Added a function to check the month

--- master_program.py
+++ master_program.py
@@ -20,12 +20,17 @@ class Master(File_management):
         logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Trash has been emptied')
         empty_trash.empty_trash()
-
+    def month_checker(self):
+        file = open('app.log', 'r')
+        if self.month_no != file.read().split('-')[1]:
+            self.empty_trash_logger()
+            #Here after emptying the trash the log file will contain two infos which are
+            #the info of sorting the trash and the info of emptying the trash
+            #Even if the program is executed only after a few months the code will work fine
+            #As the last sorting and emptying took place at almost the same time
+        file.close()
 
 if __name__ == "__main__":
     main = Master()
     main.create_dir_logger()
-    try:
-        print(main.month_no)
-    except AttributeError:
-        print('app.log created')
+    main.month_checker()

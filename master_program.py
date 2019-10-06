1bd6c74ee67d74969fb2f635550b1063c1b597b8
pranav karuvally <pranavsdreams@gmail.com>
2019-10-06 15:38:11 +0530

    Linked the logger with the create_dir.py program

--- master_program.py
+++ master_program.py
@@ -5,3 +5,9 @@ class Master(File_management):
     def logger(self):
         logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Trash has been sorted')
+
+if __name__ == "__main__":
+    main = Master()
+    main.check_dir()
+    main.categorise()
+    main.logger()

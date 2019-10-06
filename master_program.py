de9653fde0db84260c2b922c1273ef6fb2060651
pranav karuvally <pranavsdreams@gmail.com>
2019-10-06 16:11:49 +0530

    Added a feature to retrieve the month of last execution

--- master_program.py
+++ master_program.py
@@ -1,8 +1,16 @@
 import logging
 import empty_trash
+import os
 from create_dir import File_management
 
 class Master(File_management):
+    if os.path.isfile('app.log'):
+        #Here we are trying the read the month of last execution for comparison
+        file = open('app.log', 'r')
+        #file.read() return the sentence, then splitting it with '-' sperates the dates
+        #the 2nd element in the list after splitting is the month
+        month_no = file.read().split('-')[1]
+        file.close()
     def create_dir_logger(self):
         logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Trash has been sorted')
@@ -17,3 +25,7 @@ class Master(File_management):
 if __name__ == "__main__":
     main = Master()
     main.create_dir_logger()
+    try:
+        print(main.month_no)
+    except AttributeError:
+        print('app.log created')

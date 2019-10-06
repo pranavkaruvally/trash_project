6ca82dc4cb6e1bd95231a93310be0c858162988f
pranav karuvally <pranavsdreams@gmail.com>
2019-10-06 15:54:06 +0530

    added empty_trash_logger and changed logger name to create_dir_logger

--- master_program.py
+++ master_program.py
@@ -1,13 +1,19 @@
 import logging
+import empty_trash
 from create_dir import File_management
 
 class Master(File_management):
-    def logger(self):
+    def create_dir_logger(self):
         logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Trash has been sorted')
+        self.check_dir()
+        self.categorise()
+    def empty_trash_logger(self):
+        logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('Trash has been emptied')
+        empty_trash.empty_trash()
+
 
 if __name__ == "__main__":
     main = Master()
-    main.check_dir()
-    main.categorise()
-    main.logger()
+    main.create_dir_logger()

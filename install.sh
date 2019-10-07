b290329e6afda226cce061a4f6942983d6ca3c61
Pranav Karuvally <49746983+theunixdisaster@users.noreply.github.com>
2019-10-07 13:34:35 +0530

    Delete install.sh as it causes kernel panic

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo mv master_program.service /lib/systemd/system/master_program.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable master_program.service
-sudo systemctl start master_program.service

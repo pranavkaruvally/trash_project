406daac71d68580b99708c744a9e4d5a08666d7c
pranav karuvally <pranavsdreams@gmail.com>
2019-10-07 10:38:08 +0530

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo mv master_program.service /lib/systemd/system/master_program.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable dummy.service
+sudo systemctl enable master_program.service
+sudo systemctl start master_program.service
